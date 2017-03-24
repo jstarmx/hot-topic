7dfd99d3523f972952eabfa93374f3ab175a8777
James Starkie <james.starkie@onthebeach.co.uk>
2017-03-24 17:31:56 +0000

    Fix bug when voting

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 socket.open(server);
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 8000));
 app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
