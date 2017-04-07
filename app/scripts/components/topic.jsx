b6453faa629cf2d5438f08a44acc898048424814
James Starkie <james.starkie@onthebeach.co.uk>
2017-04-07 14:21:56 +0100

    code review

--- app/scripts/components/topic.jsx
+++ app/scripts/components/topic.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react';
+import { map } from 'lodash';
 
 import ScoreBar from './score_bar';
 import X from './icons/x';
 
 const Topic = ({ id, topic, red, amber, green, remove }) => {
-  const colors = [{ red }, { amber }, { green }];
+  const colors = { red, amber, green };
   const voteCount = red + amber + green;
 
   return (
@@ -15,17 +16,12 @@ const Topic = ({ id, topic, red, amber, green, remove }) => {
         </button>
         {topic.replace(/%27/g, "'")}
       </td>
-      {colors.map((color) => {
-        const name = Object.keys(color)[0];
-        const score = Object.values(color)[0];
-
-        return (
-          <td className={`dashboard__score dashboard__score--${name}`} key={name}>
-            <ScoreBar voteCount={voteCount} score={score} color={name} />
-            <span className="dashboard__score-text">{score}</span>
-          </td>
-        );
-      })}
+      {map(colors, (score, name) => (
+        <td className={`dashboard__score dashboard__score--${name}`} key={name}>
+          <ScoreBar voteCount={voteCount} score={score} color={name} />
+          <span className="dashboard__score-text">{score}</span>
+        </td>
+      ))}
     </tr>
   );
 };
